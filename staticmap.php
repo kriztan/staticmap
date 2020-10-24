2b5f3e12df9f6dfb24c1f49e16fbcdac7310aafb
Christian Schneppe <christian.schneppe@pix-art.de>
2020-10-24 13:50:20 +0200

    update map preview service

--- staticmap.php
+++ staticmap.php
@@ -319,11 +319,11 @@ Class staticMapLite
         if ($this->useTileCache && ($cached = $this->checkTileCache($url))) return $cached;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        if (!empty($_SERVER['HTTP_USER_AGENT'])) {
-        	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-        } else {
-        	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-      	}
+        //if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+        //	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        //} else {
+        	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Linux; Android 7.1.2; Nexus 7 Build/NZH54D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Safari/537.36');
+      	//}
         curl_setopt($ch, CURLOPT_URL, $url);
         $tile = curl_exec($ch);
         curl_close($ch);
@@ -355,7 +355,7 @@ Class staticMapLite
         $this->initCoords();
         $this->createBaseMap();
         if (count($this->markers)) $this->placeMarkers();
-        //if ($this->osmLogo) $this->copyrightNotice();
+        if ($this->osmLogo) $this->copyrightNotice();
     }
 
     public function showMap()
